1800a4d95aeebd54d4d722d0f99e3a2781401583
sofii2304 <nyssofia2304@gmail.com>
2023-09-15 21:15:52 -0500

    actualizacion

--- num.py
+++ num.py
@@ -1,1 +1,8 @@
 #Solicita al usuario un numero entero 
+num = float (input("Ingresa numero entero: "))
+
+#comprueba si es par o impar 
+if num % 2 == 0 :
+    print("el numero es par.")
+else:
+    print("el numero es impar.")
